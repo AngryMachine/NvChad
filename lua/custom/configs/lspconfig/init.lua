690be645695a9bc5a503b88d55d3c6711d65e9e4
chris <chrisshangguan@163.com>
2024-04-27 13:01:22 +0800

    add default lsp settings

---
+++ lua/custom/configs/lspconfig/init.lua
@@ -0,0 +1,16 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+
+-- default settings
+local servers = { "html", "cssls", "clangd" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- additional settings
+require("custom.configs.lspconfig.rust").on_setup()
---
+++ lua/custom/configs/lspconfig/rust.lua
@@ -0,0 +1,7 @@
+-- Do not call the nvim-lspconfig.rust_analyzer setup or set up the LSP client for rust-analyzer manually,
+-- as doing so may cause conflicts.
+return {
+  on_setup = function ()
+
+  end
+}
